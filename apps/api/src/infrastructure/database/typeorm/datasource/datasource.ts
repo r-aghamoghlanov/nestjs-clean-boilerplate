450839866d77ba239efc82192ed7e51f5b9bbf03
Rustam Aghamoghlanov <raghamoghlanov@gmail.com>
2025-09-02 02:42:51 +0400

    more changes

--- apps/api/src/infrastructure/database/typeorm/datasource/datasource.ts
+++
@@ -1,9 +0,0 @@
-import { DataSource } from 'typeorm';
-import { getDataSourceOptions } from './datasource-options';
-
-const createDataSourceForMigration = () => {
-  const datasource = new DataSource(getDataSourceOptions());
-  return datasource;
-};
-
-export default createDataSourceForMigration();
--- apps/api/src/infrastructure/database/typeorm/models/table-names.ts
+++
@@ -1,3 +0,0 @@
-export const TABLE_NAMES = {
-  USER: 'user',
-};

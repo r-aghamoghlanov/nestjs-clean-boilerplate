c44bdd5ff8d9d45f0515fea68981ee136693f9bf
Rustam Aghamoghlanov <raghamoghlanov@gmail.com>
2025-10-05 04:42:03 +0400

    update package jsons

---
+++ packages/infrastructure/src/database/typeorm/migrations/1759624035475-user-table-migration.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UserTableMigration1759624035475 implements MigrationInterface {
+  name = 'User_Table_Migration1759624035475';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "email" character varying NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
+}
